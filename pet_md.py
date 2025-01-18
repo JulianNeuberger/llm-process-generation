165392a298285114ff4d4ae1237905062619c876
Felix Pöschl <fel.poeschl@gmail.com>
2025-01-18 17:42:52 +0100

    added shuttle as new model

--- pet_md.py
+++ pet_md.py
@@ -26,8 +26,8 @@ if __name__ == "__main__":
     # model_name = "ollama-mistral7b-v0.2"
     # model_name = "ollama-llama3.3-70b-instruct"
     # model_name = "ollama-calme2.1-qwen2.5-72b"
-    model_name = "ollama-solar-pro-preview"
-
+    # model_name = "ollama-solar-pro-preview"
+    model_name = "ollama-shuttle-3"
     def main():
         load_dotenv()
 
