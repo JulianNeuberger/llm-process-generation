2bcc22ce3e44a74c1bbe431171c07ae36df8d0f0
Felix Pöschl <fel.poeschl@gmail.com>
2025-01-22 17:04:21 +0100

    test 2 Bit Quant

--- experiments/common.py
+++ experiments/common.py
@@ -262,6 +262,6 @@ def chat_model_for_name(model_name: str) -> BaseChatModel:
         return langchain_openai.ChatOpenAI(model_name="hf.co/MaziyarPanahi/Llama-3.3-70B-Instruct-GGUF:Q4_K_M", temperature=0,
                                            openai_api_base="http://132.180.195.1:8007/v1")
     if model_name.startswith("ollama-llama3.3-Q2"):
-        return langchain_openai.ChatOpenAI(model_name="hf.co/MaziyarPanahi/Llama-3.3-70B-Instruct-GGUF:Q2", temperature=0,
+        return langchain_openai.ChatOpenAI(model_name="hf.co/MaziyarPanahi/Llama-3.3-70B-Instruct-GGUF:Q2_K", temperature=0,
                                            openai_api_base="http://132.180.195.1:8007/v1")
     raise ValueError(f'Unknown model with name "{model_name}"')
