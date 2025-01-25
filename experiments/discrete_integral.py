7761bd4a52109eb9ffe2ce8218ddb0df81beeea6
Felix Pöschl <fel.poeschl@gmail.com>
2025-01-25 20:42:03 +0100

    adds average * runtime method for energy calc

--- experiments/discrete_integral.py
+++ experiments/discrete_integral.py
@@ -33,19 +33,30 @@ def parse_logfile(file_path: str):
 def calc_integral_trapezoid(timestamps: list[float], power_measurements: list[float]):
     microjoules = np.trapz(power_measurements, timestamps)
     joules = microjoules / 1000000
-    kwh = joules / 3600000
-    return str(kwh)
+    kwh = round(joules / 3600000, 6)
+    return kwh
+
+
+def calc_integral_power_average(timestamps: list[float], power_measurements: list[float]):
+    power_average = statistics.fmean(power_measurements)
+    microjoules = power_average * timestamps[-1]
+    joules = microjoules / 1000000
+    kwh = round(joules / 3600000, 6)
+    return kwh
 
 
 def main():
-    timestamps, power_measurements = parse_logfile("../res/power/ollama-llama3.3-70b-instruct 2025-01-25_16-23-26 "
-                                                   "pet_md.dat")
+    timestamps, power_measurements = parse_logfile("../res/power/ollama-llama3.3-70b-instruct 2025-01-25_17-06-20 "
+                                                   "pet_md-iterative1.dat")
     # conversion to seconds
-    time_elapsed = str(round(timestamps[-1] / 1000000))
-    average_power = str(statistics.fmean(power_measurements))
-    print("Experiment ran for: " + time_elapsed + " seconds." + "\n" +
-          "Average power draw: " + average_power + " W" + "\n" +
-          "Total energy used: " + calc_integral_trapezoid(timestamps, power_measurements) + " kWh")
+    time_elapsed = round(timestamps[-1] / 1000000)
+    average_power = round(statistics.fmean(power_measurements), 6)
+    print("Experiment runtime: " + str(time_elapsed) + " seconds." + "\n" +
+          "Average power draw: " + str(average_power) + " W" + "\n" +
+          "Total energy used (trapezoid method): " + str(calc_integral_trapezoid(timestamps, power_measurements))
+          + " kWh" + "\n" + "Total energy used (average power draw * runtime): " + str(
+        calc_integral_power_average(timestamps, power_measurements)) + " kWh"
+          )
 
 
 if __name__ == "__main__":
