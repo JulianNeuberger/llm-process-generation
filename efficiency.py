4a5329d7791c972a2155de763cb5689cbdd35556
Felix Pöschl <fel.poeschl@gmail.com>
2025-02-12 13:07:34 +0100

    changed model for test run

--- efficiency.py
+++ efficiency.py
@@ -414,7 +414,7 @@ if __name__ == "__main__":
 
 
     exp_type = "pet_md"
-    mod_name = "ollama-calme3.2-instruct-78b-Q4_K_S"
+    mod_name = "solar-pro-preview-instruct-GGUF:Q4_K_S"
     total_iter = 5
     date_formatted = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
 
