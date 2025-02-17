45e0e703dd83b1f6f0fc2757dd1e9cd26fb50d97
Felix Pöschl <fel.poeschl@gmail.com>
2025-02-17 12:52:35 +0100

    changed model to ultiima IQ4_XS

--- experiments/common.py
+++ experiments/common.py
@@ -264,7 +264,7 @@ def chat_model_for_name(model_name: str) -> BaseChatModel:
         return langchain_openai.ChatOpenAI(model_name=local_model_name, temperature=0.5,
                                            openai_api_base="http://132.180.195.1:8020/v1")
 
-    if model_name.startswith("ollama-calme2.1-qwen2.5-72b"):
+    if model_name.startswith("calme2.1-qwen2.5-72b"):
         return langchain_openai.ChatOpenAI(model_name="hf.co/mradermacher/calme-2.1-qwen2.5-72b-GGUF:Q4_K_M", temperature=0.5,
                                            openai_api_base="http://132.180.195.1:8007/v1")
     if model_name.startswith("solar-pro-preview-instruct-GGUF:Q4_K_M"):
@@ -273,24 +273,24 @@ def chat_model_for_name(model_name: str) -> BaseChatModel:
     if model_name.startswith("solar-pro-preview-instruct-GGUF:Q4_K_S"):
         return langchain_openai.ChatOpenAI(model_name="hf.co/MaziyarPanahi/solar-pro-preview-instruct-GGUF:Q4_K_S", temperature=0.5,
                                            openai_api_base="http://132.180.195.1:8007/v1")
-    if model_name.startswith("ollama-shuttle-3-Q4_K_S"):
+    if model_name.startswith("shuttle-3-Q4_K_S"):
         return langchain_openai.ChatOpenAI(model_name="hf.co/mradermacher/shuttle-3-GGUF:Q4_K_S", temperature=0.5,
                                            openai_api_base="http://132.180.195.1:8007/v1")
-    if model_name.startswith("ollama-llama3.3-70b-instruct-Q4_K_S"):
+    if model_name.startswith("llama3.3-70b-instruct-Q4_K_S"):
         return langchain_openai.ChatOpenAI(model_name="hf.co/mradermacher/Llama-3.3-70B-Instruct-GGUF:Q4_K_S", temperature=0.5,
                                            openai_api_base="http://132.180.195.1:8007/v1")
-    if model_name.startswith("ollama-llama3.3-70b-instruct-Q2"):
+    if model_name.startswith("llama3.3-70b-instruct-Q2"):
         return langchain_openai.ChatOpenAI(model_name="hf.co/bartowski/Llama-3.3-70B-Instruct-GGUF:Q2_K", temperature=0.5,
                                            openai_api_base="http://132.180.195.1:8007/v1")
-    if model_name.startswith("ollama-llama3.3-70b-instruct-Q3_K_S"):
+    if model_name.startswith("llama3.3-70b-instruct-Q3_K_S"):
         return langchain_openai.ChatOpenAI(model_name="hf.co/bartowski/Llama-3.3-70B-Instruct-GGUF:Q3_K_S", temperature=0.5,
                                            openai_api_base="http://132.180.195.1:8007/v1")
-    if model_name.startswith("ollama-deepseek-r1-32b"):
+    if model_name.startswith("deepseek-r1-32b"):
         return langchain_openai.ChatOpenAI(model_name="deepseek-r1:32b", temperature=0.5,
                                            openai_api_base="http://132.180.195.1:8007/v1")
-    if model_name.startswith("ollama-lamarck-14B"):
+    if model_name.startswith("lamarck-14B"):
         return langchain_openai.ChatOpenAI(model_name="hf.co/bartowski/Lamarck-14B-v0.7-GGUF:Q2_K", temperature=0.5, openai_api_base="http://132.180.195.1:8007/v1")
-    if model_name.startswith("ollama-calme3.2-instruct-78b-Q4_K_S"):
+    if model_name.startswith("calme3.2-instruct-78b-Q4_K_S"):
         return langchain_openai.ChatOpenAI(model_name="hf.co/mradermacher/calme-3.2-instruct-78b-GGUF:Q4_K_S", temperature=0.5, openai_api_base="http://132.180.195.1:8007/v1")
     if model_name.startswith("ultiima-32b-Q4_K_S"):
         return langchain_openai.ChatOpenAI(model_name="hf.co/mradermacher/ultiima-32B-GGUF:Q4_K_S", temperature=0.5, openai_api_base="http://132.180.195.1:8007/v1")
@@ -308,4 +308,6 @@ def chat_model_for_name(model_name: str) -> BaseChatModel:
         return langchain_openai.ChatOpenAI(model_name="hf.co/mradermacher/ultiima-32B-GGUF:Q6_K", temperature=0.5, openai_api_base="http://132.180.195.1:8007/v1")
     if model_name.startswith("ultiima-72b-Q2_K"):
         return langchain_openai.ChatOpenAI(model_name="hf.co/mradermacher/ultiima-72B-GGUF:Q2_K", temperature=0.5, openai_api_base="http://132.180.195.1:8007/v1")
+    if model_name.startswith("ultiima-32b-IQ4_XS"):
+        return langchain_openai.ChatOpenAI(model_name="hf.co/mradermacher/ultiima-32B-i1-GGUF:IQ4_XS", temperature=0.5, openai_api_base="http://132.180.195.1:8007/v1")
     raise ValueError(f'Unknown model with name "{model_name}"')
