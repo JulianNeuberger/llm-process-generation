545a0cba11c2e87436c0cbe2f89dba22654471d7
Felix Pöschl <fel.poeschl@gmail.com>
2025-02-19 18:08:11 +0100

    changed model to lamarckvergence IQ4_X_S

--- efficiency.py
+++ efficiency.py
@@ -430,7 +430,7 @@ if __name__ == "__main__":
             err_df_combined.to_excel(writer, sheet_name="errors", index=False)
 
     exp_type = "pet_md"
-    mod_name = "ollama-Lamarckvergence-14B-Q8_0"
+    mod_name = "ollama-Lamarckvergence-14B-IQ4_XS"
     total_iter = 5
     date_formatted = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
 
