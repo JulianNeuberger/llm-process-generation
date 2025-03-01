584ed271318e41b9cfc0e5c545cfe5d700174f2c
Felix Pöschl <fel.poeschl@gmail.com>
2025-03-01 11:00:05 +0100

    changed model distributer

--- experiments/common.py
+++ experiments/common.py
@@ -330,5 +330,5 @@ def chat_model_for_name(model_name: str) -> BaseChatModel:
     if model_name.startswith("ollama-Lamarckvergence-14B-IQ4_NL"):
         return langchain_openai.ChatOpenAI(model_name="hf.co/mradermacher/Lamarckvergence-14B-i1-GGUF:IQ4_NL", temperature=0.5, openai_api_base="http://132.180.195.1:8007/v1")
     if model_name.startswith("ollama-c4ai-command-r-plus-08-2024-Q5_K_M"):
-        return langchain_openai.ChatOpenAI(model_name="hf.co/bartowski/c4ai-command-r-plus-08-2024-GGUF:Q5_K_M", temperature=0.5, openai_api_base="http://132.180.195.1:8007/v1")
+        return langchain_openai.ChatOpenAI(model_name="command-r-plus:104b-08-2024-q5_K_M", temperature=0.5, openai_api_base="http://132.180.195.1:8007/v1")
     raise ValueError(f'Unknown model with name "{model_name}"')
