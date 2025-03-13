41f941de95dcc669617d48c49759f54e601b4c5c
Felix Pöschl <fel.poeschl@gmail.com>
2025-03-13 08:36:41 +0100

    test memory usage

--- experiments/power.py
+++ experiments/power.py
@@ -74,11 +74,11 @@ class PowerMeasurementThread(Thread):
         current_date = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")
         lines = process_output.splitlines()
         power = lines[1].split(' ')[0] if len(lines) > 1 else "error"
-
+        memory = lines[1].split(' ')[1] if len(lines) > 1 else "error"
         pathlib.Path(self.log_path).parent.mkdir(exist_ok=True, parents=True)
         with open(self.log_path, "a") as f:
             f.write(f"{current_date}\t\t{self.current_document.id if self.current_document else 'None'}\t\t"
-                    f"{self.current_fold_id if self.current_fold_id else 'None'}\t\t{power}\n")
+                    f"{self.current_fold_id if self.current_fold_id else 'None'}\t\t{power}\t{memory}\n")
 
         with self.lock:
             if self.running:
